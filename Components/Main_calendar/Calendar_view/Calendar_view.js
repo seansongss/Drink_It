0868fa8d3da9266665898117d6653353488f2fa3
seansongss <seansongss@gmail.com>
2024-01-11 16:22:59 -0500

    calendar view

--- Components/Main_calendar/Calendar_view/Calendar_view.js
+++ Components/Main_calendar/Calendar_view/Calendar_view.js
@@ -11,6 +11,7 @@ import styles from './styles';
 import Funfact_card from '../Funfact_card/Funfact_card';
 import Stat_simple from '../Stat_simple/Stat_simple';
 import Login from '../../../Pages/Login/Login';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 let customFonts = {
     'Jaldi': require('../../../assets/fonts/Jaldi-Regular.ttf'),
