7467c03637a581f8c7b6f1f031456f621cfcd2a5
seansongss <seansongss@gmail.com>
2024-05-14 08:02:29 +0900

    tabBar icon css edit

--- BottomNav.jsx
+++ BottomNav.jsx
@@ -14,31 +14,23 @@ const BottomNav = () => {
 
     return (
         <Tab.Navigator
-            sceneContainerStyle={{ backgroundColor: "#A2B69F" }}
+            backBehavior='history'
+            sceneContainerStyle={{ flex: 1, backgroundColor: "#A2B69F" }}
             screenOptions={({ route }) => ({
                 //Icon Setting
                 tabBarIcon: ({ focused, color, size }) => {
                     if (route.name === "Home") {
-                        image = focused
-                            ? require("./assets/nav_bar/home.png")
-                            : require("./assets/nav_bar/home.png");
+                        image = focused ? require("./assets/nav_bar/home.png") : require("./assets/nav_bar/home.png");
                         width = 50;
                         height = 42;
                     } else if (route.name === "My_page") {
-                        image = focused
-                            ? require("./assets/nav_bar/person.png")
-                            : require("./assets/nav_bar/person.png");
-                        width = 47;
-                        height = 45;
+                        image = focused ? require("./assets/nav_bar/person.png") : require("./assets/nav_bar/person.png");
                     } else if (route.name == "Add") {
-                        image = focused
-                            ? require("./assets/nav_bar/button.png")
-                            : require("./assets/nav_bar/button.png");
-                        width = 40;
-                        height = 40;
+                        image = focused ? require("./assets/nav_bar/button.png") : require("./assets/nav_bar/button.png");
                     }
-                    return <Image source={image} style={{ width: width, height: height, }} />
+                    return <Image source={image} resizeMode='center' style={{ width: 45, height: 45 }} />
                 },
+                tabBarItemStyle: { justifyContent:'space-evenly', marginHorizontal: 30, marginTop: 10},
                 //Show Label
                 tabBarShowLabel: false,
                 headerShown: false,
