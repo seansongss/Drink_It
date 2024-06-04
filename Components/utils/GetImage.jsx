21a73e76da42961c967d11f9752c23f7e36bfe97
seansongss <seansongss@gmail.com>
2024-06-04 20:48:37 +0900

    util functions

--- Components/utils/GetImage.jsx
+++ Components/utils/GetImage.jsx
@@ -4,13 +4,13 @@ import React, { createContext, useState, useEffect } from 'react';
 export const getAlchohol = (name) => {
 	switch (name) {
 		case "soju":
-			return require("../../../assets/alcohol/soju_logo.png");
+			require("../../assets/alcohol/soju_logo.png");
 		case "beer":
-			return require("../../../assets/alcohol/beer_logo.png");
+			return require("../../assets/alcohol/beer_logo.png");
 		case "wine":
-			return require("../../../assets/alcohol/wine_logo.png");
+			return require("../../assets/alcohol/wine_logo.png");
 		case "vodka":
-			return require("../../../assets/alcohol/vodka_logo.png");
+			return require("../../assets/alcohol/vodka_logo.png");
 		default:
 			console.error('Invalid alcohol name');
 			return null;
@@ -21,15 +21,15 @@ export const getAlchohol = (name) => {
 export const getFeeling = (feeling) => {
 	switch (feeling) {
 		case 1:
-			return require("../../../assets/Daily_view/feeling5.png");
+			return require("../../assets/Daily_view/feeling5.png");
 		case 2:
-			return require("../../../assets/Daily_view/feeling4.png");
+			return require("../../assets/Daily_view/feeling4.png");
 		case 3:
-			return require("../../../assets/Daily_view/feeling3.png");
+			return require("../../assets/Daily_view/feeling3.png");
 		case 4:
-			return require("../../../assets/Daily_view/feeling2.png");
+			return require("../../assets/Daily_view/feeling2.png");
 		case 5:
-			return require("../../../assets/Daily_view/feeling1.png");
+			return require("../../assets/Daily_view/feeling1.png");
 		default:
 			console.error('Invalid feeling number');
 			return null;
