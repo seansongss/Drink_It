553cf720816abc2ce9d762066cb7d45b35088ce7
seansongss <seansongss@gmail.com>
2024-06-05 08:03:44 +0900

    RecordHeader css

---
+++ Components/NewRecord/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    unitContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+        paddingVertical: 10,
+        marginVertical: 10,
+        borderRadius: 20,
+        borderWidth: 3,
+        backgroundColor: "white",
+    },
+    text: {
+        fontSize: 18,
+        fontFamily: 'Jaldi_700Bold',
+    },
+});
+
+export default styles;
