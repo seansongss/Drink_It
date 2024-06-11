7b63c95b88446c445d3d2e10a098ed02e52ead20
seansongss <seansongss@gmail.com>
2024-06-11 16:57:01 +0900

    mount animatedscreen to app

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Constants from 'expo-constants';
+import * as SplashScreen from 'expo-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
-import BottomNav from './BottomNav';
 import { useFonts, Jaldi_400Regular, Jaldi_700Bold } from '@expo-google-fonts/jaldi';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import * as SplashScreen from 'expo-splash-screen';
+import BottomNav from './BottomNav';
 
 // Keep the splash screen visible while we fetch resources
 SplashScreen.preventAutoHideAsync();
@@ -37,11 +38,13 @@ export default function App() {
     });
 
     return (
-        <SafeAreaProvider style={{ flex: 1, backgroundColor: '#A2B69F' }}>
-            <NavigationContainer style={{ flex: 1 }}>
-                <BottomNav />
-            </NavigationContainer>
-        </SafeAreaProvider>
+        <AnimatedSplashScreen>
+            <SafeAreaProvider style={{ flex: 1, backgroundColor: '#A2B69F' }}>
+                <NavigationContainer style={{ flex: 1 }}>
+                    <BottomNav />
+                </NavigationContainer>
+            </SafeAreaProvider>
+        </AnimatedSplashScreen>
     );
 }
 
