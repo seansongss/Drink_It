f8eb814a9512dffc2b60d5f17a1ca1220ac6fec2
seansongss <seansongss@gmail.com>
2024-06-16 14:00:02 +0900

    delete comments for ImageComponent

--- Components/utils/ImageComponent.jsx
+++ Components/utils/ImageComponent.jsx
@@ -53,8 +53,6 @@ const ImageComponent = React.memo(({ type, value, size = 30 }) => {
   return (
     <Image
       style={{ width: size, height: size }}
-      // placeholder={source}
-      // placeholderContentFit="contain"
       source={source}
       contentFit="contain"
     />
