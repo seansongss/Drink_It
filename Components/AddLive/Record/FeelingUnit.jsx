915253898c5d274f62423f8945b39fb422cc2070
seansongss <seansongss@gmail.com>
2024-06-16 17:01:21 +0900

    Commented FeelingUnit

--- Components/AddLive/Record/FeelingUnit.jsx
+++ Components/AddLive/Record/FeelingUnit.jsx
@@ -5,8 +5,7 @@ import ImageComponent from '../../utils/ImageComponent';
 import styles from './styles';
 
 const FeelingUnit = memo(({ name, feelingValue, changeFeeling }) => {
-    // const feelingValue = feelings[name];
-    console.log('FeelingUnit rendered for', name, 'value:', feelingValue);
+    // console.log('FeelingUnit rendered for', name, 'value:', feelingValue);
     return (
         <View style={styles.addFeeling}>
             <TouchableOpacity
