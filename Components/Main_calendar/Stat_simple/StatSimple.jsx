474463f7684f5c8963df3dca7f74465b74001984
seansongss <seansongss@gmail.com>
2024-06-28 23:07:16 +0900

    add progressbar to StatSimple

--- Components/Main_calendar/Stat_simple/StatSimple.jsx
+++ Components/Main_calendar/Stat_simple/StatSimple.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import { View } from 'react-native'
+import ProgressBar from '../../utils/ProgressBar'
+import ProgressBar2 from '../../utils/ProgressBar2'
 
 import styles from './styles'
 
 const StatSimple = () => {
     return (
         <View style={styles.statContainer}>
-            <View style={styles.statBox} />
+            {/* <View style={styles.statBox} /> */}
+            {/* <View style={styles.statBox}>
+                <ProgressBar
+                    shouldAnimate={true} // to enable animation, default false
+                    animateDuration={1000} // if animation enabled
+                    data={[
+                        { progress: 7, color: "rgb(255, 193, 2)" },
+                        { progress: 13, color: "rgb(55, 106, 255)" },
+                        { progress: 5, color: "rgb(229, 232, 249)" }
+                    ]}
+                />
+            </View> */}
+            <ProgressBar2
+                shouldAnimate={true} // to enable animation, default false
+                animateDuration={1000} // if animation enabled
+                data={[
+                    { progress: 7, color: "rgb(255, 193, 2)" },
+                    { progress: 13, color: "rgb(55, 106, 255)" },
+                    { progress: 5, color: "rgb(229, 232, 249)" }
+                ]}
+            />
             <View style={styles.statBox} />
             <View style={styles.statBox} />
         </View>
