d42f4029c27a8d1bd1cb45e885a177011b92a262
seansongss <seansongss@gmail.com>
2024-06-30 13:33:18 +0900

    ProgressBar done

--- Components/utils/ProgressBar.jsx
+++ Components/utils/ProgressBar.jsx
@@ -1,18 +1,11 @@
 import { useEffect, useState, useMemo } from "react";
 import { View, Animated, Easing } from "react-native";
 
-const ProgressBar = ({ data, animateDuration = 1000, barHeight = 8, shouldAnimate = true, style }) => {
+const ProgressBar = ({ data, animateDuration = 1000, shouldAnimate = true, style }) => {
     const [progressData, setProgressData] = useState([]);
     const animation = useMemo(() => new Animated.Value(0), []);
 
     useEffect(() => {
-        Animated.timing(animation, {
-            toValue: 1,
-            duration: 1000,
-            // easing: Easing.linear,
-            useNativeDriver: true,
-        }).start();
-
         let totalProgress = data.reduce((acc, d) => acc + d.progress, 0);
         let value = 0;
         let processedData = data.map(d => {
@@ -24,7 +17,15 @@ const ProgressBar = ({ data, animateDuration = 1000, barHeight = 8, shouldAnimat
         });
 
         setProgressData(processedData);
-    }, []);
+
+        // Start the animation after setting the processed data
+        Animated.timing(animation, {
+            toValue: 1,
+            duration: animateDuration,
+            easing: Easing.linear,
+            useNativeDriver: false, // Set to false since we're animating width
+        }).start();
+    }, [data, animateDuration, animation]);
 
     const animatedValues = progressData.map(d => {
         return animation.interpolate({
@@ -33,7 +34,6 @@ const ProgressBar = ({ data, animateDuration = 1000, barHeight = 8, shouldAnimat
         });
     });
 
-    console.log(data);
     console.log(progressData);
 
     return (
@@ -42,27 +42,20 @@ const ProgressBar = ({ data, animateDuration = 1000, barHeight = 8, shouldAnimat
                 {
                     display: "flex",
                     flexDirection: "row",
-                    marginTop: 16,
-                    marginBottom: 16 + barHeight,
-                    width: "100%",
-                    height: barHeight,
-                    borderRadius: 5,
                     borderWidth: 1,
+                    overflow: 'hidden' // Ensure children do not overflow the rounded container
                 },
                 style
             ]}
         >
             {progressData.map((d, i) => {
-                console.log(d.progress * 100);
                 return (
                     <Animated.View
                         key={i}
                         style={{
-                            display: "flex",
-                            flex: d.progress,
-                            height: barHeight || 8,
+                            width: animatedValues[i],
+                            height: '100%',
                             backgroundColor: d.color,
-                            opacity: animation
                         }}
                     />
                 );
