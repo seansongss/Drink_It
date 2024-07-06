772886a11503bfb3bc01055fd12fb8fdd36590c4
seansongss <seansongss@gmail.com>
2024-07-06 17:41:59 +0900

    add SocialButton component

---
+++ Components/Login/SocialButton/styles.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    buttonContainer: {
+        width: 60,
+        height: 60,
+        borderRadius: 30,
+        margin: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#f2f2f2',
+    },
+});
+
+export default styles;
