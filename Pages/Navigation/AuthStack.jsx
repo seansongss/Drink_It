26681bf17a6e1757464790fa5dd99af36857d9a6
seansongss <seansongss@gmail.com>
2024-07-09 13:07:05 +0900

    AuthStack

--- Pages/Navigation/AuthStack.jsx
+++ Pages/Navigation/AuthStack.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Login from '../Login/Login';
 import SignUp from '../../Components/Authentication/SignUp/SignUp';
+import LogIn from '../../Components/Authentication/LogIn/LogIn';
 
 const Stack = createNativeStackNavigator();
 
 function AuthStack() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="DailyView" component={Login} />
-            <Stack.Screen name="NewRecord" component={SignUp} />
+            <Stack.Screen name="LogIn" component={LogIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
         </Stack.Navigator>
     );
 }
