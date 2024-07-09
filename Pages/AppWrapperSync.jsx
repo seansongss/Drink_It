929757c79dcec341622ec95e3157ae13b7e31d50
seansongss <seansongss@gmail.com>
2024-07-09 11:04:16 +0900

    Edit AppWrapper

--- Pages/AppWrapperSync.jsx
+++ Pages/AppWrapperSync.jsx
@@ -5,13 +5,13 @@ import { SYNC_CONFIG } from '../sync.config';
 import BottomNav from '../BottomNav';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import Login from './Login/Login';
+import AuthStack from './Navigation/AuthStack';
 
 
 const AppWrapperSync = () => {
     return (
         <AppProvider id={SYNC_CONFIG.appId}>
-            <UserProvider fallback={Login}>
+            <UserProvider fallback={AuthStack}>
                 <RealmProvider>
                     <SafeAreaProvider style={{ flex: 1, backgroundColor: '#A2B69F' }}>
                         <NavigationContainer style={{ flex: 1 }}>
