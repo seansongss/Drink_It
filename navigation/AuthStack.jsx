ab83757e2c244e50be2c60a8aa2b92abdcd64570
seansongss <seansongss@gmail.com>
2024-07-10 22:27:10 +0900

    edit path

--- navigation/AuthStack.jsx
+++ navigation/AuthStack.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SignUp from '../components/Authentication/SignUp/SignUp';
-import LogIn from '../components/Authentication/LogIn/LogIn';
+import SignUp from '../pages/SignUp/SignUp';
+import LogIn from '../pages/LogIn/LogIn';
 
 const Stack = createNativeStackNavigator();
 
