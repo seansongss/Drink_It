088149a497ba8261fefdc6c77ec241035678f09a
seansongss <seansongss@gmail.com>
2024-07-12 08:00:59 +0900

    add react-native-google-signin

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,22 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
+      // react-native-dotenv
+      [
+        'module:react-native-dotenv',
+        {
+          "envName": "APP_ENV",
+          "moduleName": "@env",
+          "path": ".env.local",
+          "blocklist": null,
+          "allowlist": null,
+          "blacklist": null,
+          "whitelist": null,
+          "safe": false,
+          "allowUndefined": true,
+          "verbose": false
+        },
+      ],
       [
         'module-resolver',
         {
