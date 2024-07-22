f3ad19a3382efc8b3bfc8c9aa030ea9c8878509e
seansongss <seansongss@gmail.com>
2024-07-23 07:58:11 +0900

    edit AppWrapper

--- Pages/AppWrapperSync.jsx
+++ Pages/AppWrapperSync.jsx
@@ -8,43 +8,37 @@ import BottomNav from '../navigation/BottomNav';
 import AuthStack from '../navigation/AuthStack';
 import { recipeTestSchema } from 'models/schemas';
 import { recipeTest } from 'models/models';
-import { ActivityIndicator, Alert, View } from 'react-native';
 import { OpenRealmBehaviorType } from 'realm';
 
 const AppWrapperSync = () => {
     return (
-        <AppProvider id={REALM_CONFIG.appId}>
-            <UserProvider fallback={AuthStack}>
-                <RealmProvider
-                    schema={[recipeTestSchema]}
-                    sync={{
-                        flexible: true,
-                        initialSubscriptions: {
-                            update(subs, realm) {
-                                subs.add(realm.objects('recipeTest'), { name: 'allRecipes' });
+        <NavigationContainer>
+            <AppProvider id={REALM_CONFIG.appId}>
+                <UserProvider fallback={AuthStack}>
+                    <RealmProvider
+                        schema={[recipeTestSchema]}
+                        sync={{
+                            flexible: true,
+                            initialSubscriptions: {
+                                update(subs, realm) {
+                                    subs.add(realm.objects('recipeTest'), { name: 'allRecipes' });
+                                },
                             },
-                        },
-                        // newRealmFileBehavior: {
-                        //     type: OpenRealmBehaviorType.OpenImmediately,
-                        // },
-                        // existingRealmFileBehavior: {
-                        //     type: OpenRealmBehaviorType.OpenImmediately,
-                        // },
-                    }}
-                    fallback={
-                        <View>
-                            <ActivityIndicator />
-                        </View>
-                    }
-                >
-                    <NavigationContainer>
+                            newRealmFileBehavior: {
+                                type: OpenRealmBehaviorType.OpenImmediately,
+                            },
+                            existingRealmFileBehavior: {
+                                type: OpenRealmBehaviorType.OpenImmediately,
+                            },
+                        }}
+                    >
                         <SafeAreaProvider style={{ flex: 1, backgroundColor: '#A2B69F' }}>
                             <BottomNav />
                         </SafeAreaProvider>
-                    </NavigationContainer>
-                </RealmProvider>
-            </UserProvider>
-        </AppProvider>
+                    </RealmProvider>
+                </UserProvider>
+            </AppProvider>
+        </NavigationContainer>
     )
 }
 
