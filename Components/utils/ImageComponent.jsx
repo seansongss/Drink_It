a22e86f03f40d3daca3d62b6913e69ac6c0dff05
seansongss <seansongss@gmail.com>
2024-07-24 07:59:48 +0900

    add whale badge and add style option

--- Components/utils/ImageComponent.jsx
+++ Components/utils/ImageComponent.jsx
@@ -12,42 +12,49 @@ export const getImage = (type, value) => {
       }
     case 'alcohol':
       switch (value) {
-        case 'soju': return require('../../assets/alcohol/soju_logo.png');
-        case 'beer': return require('../../assets/alcohol/beer_logo.png');
-        case 'wine': return require('../../assets/alcohol/wine_logo.png');
-        case 'vodka': return require('../../assets/alcohol/vodka_logo.png');
+        case 'soju': return require('@assets/alcohol/soju_logo.png');
+        case 'beer': return require('@assets/alcohol/beer_logo.png');
+        case 'wine': return require('@assets/alcohol/wine_logo.png');
+        case 'vodka': return require('@assets/alcohol/vodka_logo.png');
         default:
           console.error('Invalid alcohol name:', value);
           return null;
       }
     case 'feeling':
       switch (value) {
-        case 1: return require('../../assets/feeling/feeling5.png');
-        case 2: return require('../../assets/feeling/feeling4.png');
-        case 3: return require('../../assets/feeling/feeling3.png');
-        case 4: return require('../../assets/feeling/feeling2.png');
-        case 5: return require('../../assets/feeling/feeling1.png');
+        case 1: return require('@assets/feeling/feeling5.png');
+        case 2: return require('@assets/feeling/feeling4.png');
+        case 3: return require('@assets/feeling/feeling3.png');
+        case 4: return require('@assets/feeling/feeling2.png');
+        case 5: return require('@assets/feeling/feeling1.png');
         default:
           console.error('Invalid feeling number:', value);
           return null;
       }
     case 'calendar':
       switch (value) {
-        case 'last_night': return require('../../assets/calendar/last_night.png');
-        case 'rankings': return require('../../assets/calendar/rankings.png');
-        case 'clock': return require('../../assets/calendar/clock.png');
-        case 'location': return require('../../assets/calendar/mapPin.png');
-        case 'note': return require('../../assets/calendar/note.png');
+        case 'last_night': return require('@assets/calendar/last_night.png');
+        case 'rankings': return require('@assets/calendar/rankings.png');
+        case 'clock': return require('@assets/calendar/clock.png');
+        case 'location': return require('@assets/calendar/mapPin.png');
+        case 'note': return require('@assets/calendar/note.png');
         default:
           console.error('Invalid calendar icon:', value);
           return null;
       }
     case 'social':
       switch (value) {
-        case 'google': return require('../../assets/social/google.png');
-        case 'apple': return require('../../assets/social/apple.png');
+        case 'google': return require('@assets/social/google.png');
+        case 'apple': return require('@assets/social/apple.png');
         default:
           console.error('Invalid social icon:', value);
+          return null;
+      }
+    case 'badge':
+      switch (value) {
+        case 'whale': return require('@assets/badge/whale.png');
+        default:
+          console.error('Invalid badge name:', value);
           return null;
       }
     default:
@@ -56,7 +63,7 @@ export const getImage = (type, value) => {
   }
 };
 
-const ImageComponent = memo(({ type, value, size = 30 }) => {
+const ImageComponent = memo(({ style, type, value, size = 30 }) => {
   const source = getImage(type, value);
 
   if (!source) {
@@ -67,7 +74,7 @@ const ImageComponent = memo(({ type, value, size = 30 }) => {
 
   return (
     <Image
-      style={{ width: size, height: size }}
+      style={[style, { width: size, height: size }]}
       source={source}
       contentFit="contain"
     />
