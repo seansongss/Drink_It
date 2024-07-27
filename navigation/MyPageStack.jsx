186f0adea28177176ad3d60ff1172c9624579628
seansongss <seansongss@gmail.com>
2024-07-27 09:17:56 +0900

    create stack tab

--- navigation/MyPageStack.jsx
+++ navigation/MyPageStack.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import SignUp from '../pages/SignUp/SignUp';
-import LogIn from '../pages/LogIn/LogIn';
-import PasswordReset from '../pages/PasswordReset/PasswordReset';
+import MyPage from '@pages/MyPage/MyPage';
+import SettingView from '@pages/SettingView/SettingView';
 
 const Stack = createNativeStackNavigator();
 
-function AuthStack() {
+function MyPageStack() {
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="LogIn" component={LogIn} />
-            <Stack.Screen name="SignUp" component={SignUp} />
-            <Stack.Screen name="PasswordReset" component={PasswordReset} />
+            <Stack.Screen name="MyPage" component={MyPage} />
+            <Stack.Screen name="SettingView" component={SettingView} />
         </Stack.Navigator>
     );
 }
 
-export default AuthStack;
+export default MyPageStack;
