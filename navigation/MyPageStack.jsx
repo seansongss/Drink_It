65f8d6d4e0885e5e4d5228a9875b7d7cdb160590
seansongss <seansongss@gmail.com>
2024-07-27 09:05:23 +0900

    add mypage stack navigation

---
+++ navigation/MyPageStack.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SignUp from '../pages/SignUp/SignUp';
+import LogIn from '../pages/LogIn/LogIn';
+import PasswordReset from '../pages/PasswordReset/PasswordReset';
+
+const Stack = createNativeStackNavigator();
+
+function AuthStack() {
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="LogIn" component={LogIn} />
+            <Stack.Screen name="SignUp" component={SignUp} />
+            <Stack.Screen name="PasswordReset" component={PasswordReset} />
+        </Stack.Navigator>
+    );
+}
+
+export default AuthStack;
