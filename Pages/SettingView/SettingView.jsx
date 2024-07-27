e6343c2ad973514ff00baab73d58d8757aae034d
seansongss <seansongss@gmail.com>
2024-07-27 15:59:28 +0900

    create SettingView

--- Pages/SettingView/SettingView.jsx
+++ Pages/SettingView/SettingView.jsx
@@ -1,32 +1,131 @@
 import React from 'react'
-import { View, Text, TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity, ScrollView, Alert } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { Icon } from "@rneui/base";
-import { MaterialIcons } from '@expo/vector-icons';
+import { Icon, Divider } from "@rneui/base";
+import { MaterialIcons, Ionicons } from '@expo/vector-icons';
+import { useUser } from '@realm/react';
+
+import ImageComponent from '@components/utils/ImageComponent'
 
 import styles from './styles'
 
 const SettingView = ({ navigation }) => {
+    const user = useUser();
+
+    const logOutButton = () => {
+        Alert.alert('Logout', 'Are you sure you want to log out?', [
+            {
+                text: 'No',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+            },
+            {
+                text: 'Yes',
+                onPress: () => user.logOut(),
+                style: 'destructive'
+            },
+        ]);
+    };
+
     return (
-        <SafeAreaView style={styles.container}>
-            <View>
-                <Text>hello</Text>
+        <SafeAreaView edges={["top"]} style={styles.container}>
+            <View style={styles.header}>
                 <TouchableOpacity
+                    style={styles.backButton}
                     onPress={() => navigation.navigate('MyPage')}
                 >
                     <MaterialIcons
                         name='keyboard-arrow-left'
-                        size={50}
-                        color="black"
-                        style={[styles.dailyViewButton]}
+                        size={70}
+                        color="#fff9d4"
                     />
                 </TouchableOpacity>
-
-                <Icon name="settings" color="white" size={40} />
+                <View style={styles.settingIcon}>
+                    <Icon name="settings" color="white" size={50} />
+                </View>
             </View>
-            <View styles={styles.body}>
-                <Text>Setting</Text>
-            </View>
+            <ScrollView
+                style={styles.contentContainer}
+            >
+                <View style={styles.personalSetting}>
+                    <Ionicons name="person" size={45} color={"#b2d8b2"} />
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>PROFILE</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>PERSONAL INFO</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>SECURITY</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                </View>
+                <Divider color='white' width={5} style={styles.divider} />
+                <View style={styles.personalSetting}>
+                    <Ionicons name="person" size={45} color={"#b2d8b2"} />
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>LAGUAGE</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>NOTIFICATIONS</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.settingWrapper}
+                    >
+                        <Text style={styles.text}>THEMES</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={[styles.settingWrapper, {marginBottom: 50}]}
+                        onPress={logOutButton}
+                    >
+                        <Text style={styles.text}>LOGOUT</Text>
+                        <MaterialIcons
+                            name='keyboard-arrow-right'
+                            size={50}
+                            color="#b2d8b2"
+                        />
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
--- Pages/SettingView/styles.js
+++ Pages/SettingView/styles.js
@@ -3,24 +3,46 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: "center",
         backgroundColor: "#b2d8b2",
     },
-    userContainer: {
-        flex: 1,
+    header: {
+        height: "13%",
         flexDirection: "row",
-        justifyContent: "center",
         alignItems: "center",
-        paddingVertical: 10,
-        marginLeft: -15,
-        marginBottom: 10,
-        backgroundColor: "black",
     },
-    body: {
+    backButton: {
+        position: "absolute",
+        zIndex: 1,
+        left: "5%",
+    },
+    settingIcon: {
         flex: 1,
-        width: "100%",
-        height: "50%",
+        alignItems: 'center',
+    },
+    contentContainer: {
+        flex: 1,
         backgroundColor: "#e6eef4",
+        paddingTop: 10,
+    },
+    personalSetting: {
+        marginHorizontal: "15%",
+        gap: 10,
+        marginTop: 15,
+        // borderWidth: 1,
+    },
+    divider: {
+        marginHorizontal: "7%",
+        marginVertical: 30,
+        borderRadius: 10,
+    },
+    settingWrapper: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+    text: {
+        fontSize: 22,
+        fontFamily: "Jaldi_400Regular",
     },
 });
 
