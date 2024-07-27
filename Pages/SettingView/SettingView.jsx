2e6a3dfa1c8d37283f1ee1125ee68b3770648261
seansongss <seansongss@gmail.com>
2024-07-27 09:05:32 +0900

    create SettingView

---
+++ Pages/SettingView/SettingView.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import styles from './styles'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { View } from 'react-native'
+
+const SettingView = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+        <View styles={styles.header}>
+    
+        </View>
+        <View styles={styles.body}>
+
+        </View>
+    </SafeAreaView>
+  )
+}
+
+export default SettingView
