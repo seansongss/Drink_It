0378100ef337acaff32288d1fb35cee2dcd7b60d
seansongss <seansongss@gmail.com>
2024-07-28 11:20:09 +0900

    RankingDropdown wip

---
+++ Components/RecipeRanking/RankingDropdown/RankingDropdown.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const RankingDropdown = ({titleList, }) => {
+  return (
+    <View>
+        
+    </View>
+  )
+}
+
+export default RankingDropdown
