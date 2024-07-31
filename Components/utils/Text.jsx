d8a7c153d878f0cdf2405b4f1ccc79642fe92698
seansongss <seansongss@gmail.com>
2024-07-31 13:13:06 +0900

    update custom Text

--- Components/utils/Text.jsx
+++ Components/utils/Text.jsx
@@ -1,15 +1,17 @@
+import { color } from '@rneui/base';
 import React from 'react';
 import { Text as RNText } from 'react-native';
 
 const Text = ({ font = "Jaldi", fontWeight = 'normal', fontSize = 18, children, style }) => {
     const fontStyles = {
         fontSize: fontSize,
+        color: 'black',
     };
 
     switch (font) {
         case 'Jaldi':
             if (fontWeight === 'bold') {
-                fontStyles.fontFamily = "Jaldi700Bold";
+                fontStyles.fontFamily = "Jaldi_700Bold";
             } else {
                 fontStyles.fontFamily = "Jaldi_400Regular";
             }
