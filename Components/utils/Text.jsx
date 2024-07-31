3c1adebcb75d48fdf41fc4f19911545e207d06b3
seansongss <seansongss@gmail.com>
2024-07-31 10:51:30 +0900

    Update Text component

--- Components/utils/Text.jsx
+++ Components/utils/Text.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import { Text } from 'react-native'
+import React from 'react';
+import { Text as RNText } from 'react-native';
 
-const Text = ({font = "Jaldi_400Regular", size = 16, children, styles}) => {
-    fontStyles = {
-        fontSize: size,
+const Text = ({ font = "Jaldi", fontWeight = 'normal', fontSize = 18, children, style }) => {
+    const fontStyles = {
+        fontSize: fontSize,
     };
 
-    switch(font) {
+    switch (font) {
         case 'Jaldi':
-
+            if (fontWeight === 'bold') {
+                fontStyles.fontFamily = "Jaldi700Bold";
+            } else {
+                fontStyles.fontFamily = "Jaldi_400Regular";
+            }
+            break;
+        default:
+            fontStyles.fontFamily = "Jaldi_400Regular";
+            break;
     }
-  
+
     return (
-    <Text style={styles}>
-        {children}
-    </Text>
-  )
+        <RNText style={[fontStyles, style]}>
+            {children}
+        </RNText>
+    );
 }
 
-export default Text
+export default Text;
