6b12c59bd84b17625a588789a06e9220dcf5a262
Alex Lee <1128alex@gmail.com>
2024-09-07 06:23:16 -0400

    On branch database  Changes to be committed: 	modified:   models/models.ts 	modified:   models/schemas.ts

--- models/models.ts
+++ models/models.ts
@@ -1,13 +1,37 @@
+import Realm from "realm";
+
+export type like = {
+  _id: Realm.BSON.ObjectId;
+  createdAt: Date;
+  recipeId?: recipe;
+  userId?: user;
+};
+
+export type recipe = {
+  _id: Realm.BSON.ObjectId;
+  createdAt: Date;
+  defaultSize: Realm.BSON.Decimal128;
+  description?: string;
+  like?: number;
+  liquorList: Realm.List<number>;
+  name: string;
+  percent: Realm.BSON.Decimal128;
+  ratio: Realm.List<Realm.BSON.Decimal128>;
+  type: number;
+  updatedAt: Date;
+  userId: number;
+};
+
 export type user = {
-    _id: Realm.BSON.ObjectId;
-    createdAt: Date;
-    dateOfBirth?: Date;
-    gender?: string;
-    isFirstTime: boolean;
-    modifiedAt?: Date;
-    preference?: user_preference;
-    user_id: string;
-    username?: string;
+  _id: Realm.BSON.ObjectId;
+  createdAt: Date;
+  dateOfBirth?: Date;
+  gender?: string;
+  isFirstTime: boolean;
+  modifiedAt?: Date;
+  preference?: user_preference;
+  user_id: string;
+  username?: string;
 };
 
 export type user_preference = {
@@ -15,13 +39,16 @@ export type user_preference = {
   preferRegion?: string;
 };
 
-export type recipeTest = {
-  _id: Realm.BSON.ObjectId;
-  alcohol: number;
+export type record = {
+  _id?: Realm.BSON.ObjectId;
+  condition: number;
+  count: Realm.List<number>;
   createdAt: Date;
-  creator: string;
-  description?: string;
-  modifiedAt?: Date;
-  recipeName: string;
-  recipeType: string;
+  endTime: Date;
+  location?: string;
+  memo?: string;
+  recipeList: Realm.List<number>;
+  startTime: Date;
+  updatedAt: Date;
+  userId?: user;
 };
--- models/schemas.ts
+++ models/schemas.ts
@@ -38,3 +38,50 @@ export const recipeTestSchema = {
     primaryKey: '_id',
 };
 
+export const likeSchema = {
+    name: 'like',
+    properties: {
+        _id: 'objectId',
+        createdAt: 'date',
+        recipeId: 'recipe',
+        userId: 'user',
+    },
+    primaryKey: '_id',
+};
+
+export const recipeSchema = {
+    name: 'recipe',
+    properties: {
+        _id: 'objectId',
+        createdAt: 'date',
+        defaultSize: 'decimal128',
+        description: 'string?',
+        like: 'int?',
+        liquorList: 'int[]',
+        name: 'string',
+        percent: 'decimal128',
+        ratio: 'decimal128[]',
+        type: 'int',
+        updatedAt: 'date',
+        userId: 'int',
+    },
+    primaryKey: '_id',
+};
+
+export const recordSchema = {
+    name: 'record',
+    properties: {
+        _id: 'objectId?',
+        condition: 'int',
+        count: 'int[]',
+        createdAt: 'date',
+        endTime: 'date',
+        location: 'string?',
+        memo: 'string?',
+        recipeList: 'int[]',
+        startTime: 'date',
+        updatedAt: 'date',
+        userId: 'user',
+    },
+    primaryKey: '_id',
+};
