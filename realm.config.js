fe2037cf8677972ffa6212d7c60070521640caa6
seansongss <seansongss@gmail.com>
2024-11-11 15:07:34 +0900

    delete realm

--- realm.config.js
+++
@@ -1,6 +0,0 @@
-export const REALM_CONFIG = {
-  // Set `enabled` to `true` to enable sync.
-  enabled: true,
-  // Add your Realm App ID here if sync is enabled.
-  appId: 'application-0-bnapwra',
-};
